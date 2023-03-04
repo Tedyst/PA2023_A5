5a8f4eb54071419f26b52458ad81e6c613809c50
Stoica Tedy <stoicatedy@gmail.com>
2023-03-04 16:06:01 +0200

    Added docs for Lab2

---
+++ Lab2/docs/package-search-index.js
@@ -0,0 +1,4 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "url": "allpackages-index.html"
+}, {"l": "ro.tedyst"}, {"l": "ro.tedyst.location"}, {"l": "ro.tedyst.road"}]
---
+++ Lab2/docs/type-search-index.js
@@ -0,0 +1,16 @@
+typeSearchIndex = [{"p": "ro.tedyst.location", "l": "Airport"}, {
+    "l": "All Classes",
+    "url": "allclasses-index.html"
+}, {"p": "ro.tedyst.location", "l": "City"}, {"p": "ro.tedyst.road", "l": "ExpressRoad"}, {
+    "p": "ro.tedyst.location",
+    "l": "GasStation"
+}, {"p": "ro.tedyst.road", "l": "HighwayRoad"}, {"p": "ro.tedyst.location", "l": "Location"}, {
+    "p": "ro.tedyst",
+    "l": "LocationType"
+}, {"p": "ro.tedyst", "l": "Main"}, {"p": "ro.tedyst", "l": "Problem"}, {
+    "p": "ro.tedyst",
+    "l": "ProblemGenerator"
+}, {"p": "ro.tedyst.road", "l": "Road"}, {"p": "ro.tedyst", "l": "RoadType"}, {
+    "p": "ro.tedyst",
+    "l": "Solution"
+}, {"p": "ro.tedyst", "l": "SolutionLocationComparator"}]
